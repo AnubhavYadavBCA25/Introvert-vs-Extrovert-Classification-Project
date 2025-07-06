822fd4b02e0db8aba8890cd8aca8391187e16687
Anubhav Yadav <ay6666@srmist.edu.in>
2025-07-06 21:04:36 +0530

    data ingestion completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,57 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger.logging import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    """
+    Data Ingestion Configuration Class
+    """
+    train_data_path: str = os.path.join('artifacts','train.csv')
+    test_data_path: str = os.path.join('artifacts','test.csv')
+    raw_data_path: str = os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    """
+    Data Ingestion Class
+    Handles the ingestion of data from a source, splits it into training and testing sets,
+    and saves the data to specified paths.
+    """
+    
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
+        try:
+            df = pd.read_csv(r'notebook\data\augmented_dataset.csv')
+            logging.info("Data loaded successfully")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info("Raw data saved")
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False)
+
+            logging.info("Train and test data saved successfully")
+            return (
+                self.ingestion_config.train_data_path, 
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__ == "__main__":
+    data_ingestion = DataIngestion()
+    train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+    print(f"Train data saved at: {train_data_path}")
+    print(f"Test data saved at: {test_data_path}")
