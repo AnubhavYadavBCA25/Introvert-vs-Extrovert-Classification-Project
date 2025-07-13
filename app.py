7523d15662566650fd16b5c44acfc0db41e95982
Anubhav Yadav <ay6666@srmist.edu.in>
2025-07-13 13:36:43 +0530

    prediction pipeline completed and predict & result page completed

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+from src.exception import CustomException
+import sys
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -7,6 +10,42 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            data = CustomData(
+                time_spent_alone=float(request.form.get("time_spent_alone")),
+                stage_fear=str(request.form.get("stage_fear")),
+                social_event_attendance=int(request.form.get("social_event_attendance")),
+                going_outside=int(request.form.get("going_outside")),
+                drained_after_socializing=str(request.form.get("drained_after_socializing")),
+                friends_circle_size=int(request.form.get("friends_circle_size")),
+                post_frequency=int(request.form.get("post_frequency"))
+            )
+
+            input_df = data.get_data_as_df()
+            print(input_df)
+
+            predict_pipeline = PredictPipeline()
+            prediction = predict_pipeline.predict(input_df)
+            predicted_class = prediction[0]
+
+            if predicted_class == 0:
+                predicted_class = 'Extrovert'
+            elif predicted_class == 1:
+                predicted_class = 'Introvert'
+            else:
+                predicted_class = 'Unknown'
+
+            return render_template('result.html', prediction=predicted_class)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+    
+    elif request.method == 'GET':
+        return render_template('predict.html')
 
 @app.route('/about')
 def about():
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -19,3 +19,40 @@ class PredictPipeline:
         
         except Exception as e:
             raise CustomException(e, sys)
+
+class CustomData:
+    def __init__(
+        self,
+        time_spent_alone: float,
+        stage_fear: str,
+        social_event_attendance: int,
+        going_outside: int,
+        drained_after_socializing: str,
+        friends_circle_size: int,
+        post_frequency: int
+        ):
+
+        self.time_spent_alone = time_spent_alone
+        self.stage_fear = stage_fear
+        self.social_event_attendance = social_event_attendance
+        self.going_outside = going_outside
+        self.drained_after_socializing = drained_after_socializing
+        self.friends_circle_size = friends_circle_size
+        self.post_frequency = post_frequency
+
+    def get_data_as_df(self):
+        try:
+            data_dict = {
+                "Time_spent_Alone": [self.time_spent_alone],
+                "Stage_fear": [self.stage_fear],
+                "Social_event_attendance": [self.social_event_attendance],
+                "Going_outside": [self.going_outside],
+                "Drained_after_socializing": [self.drained_after_socializing],
+                "Friends_circle_size": [self.friends_circle_size],
+                "Post_frequency": [self.post_frequency]
+            }
+
+            return pd.DataFrame(data_dict)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
